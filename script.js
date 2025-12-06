a57f582f7ab78bb57690381579c5113ecc28a746
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2025-12-06 06:34:25 +0200

    Finish project with the minimum requirements.

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ let submitBtn = document.querySelector("#submit-btn");
 let gridContainer = document.querySelector("#grid-container");
 
 function createGrid() {
-  if (dimention.value == "")
-     dimention.value = 16;
+  if (dimention.value == "") dimention.value = 16;
   // remove the old grid if existed
   while (gridContainer.hasChildNodes()) {
     gridContainer.removeChild(gridContainer.firstChild);
@@ -19,6 +18,15 @@ function createGrid() {
   while (gridSize--) {
     let gridElement = document.createElement("div");
     gridElement.classList.add("grid-element");
+
+    gridElement.addEventListener(
+      "mouseover",
+      () => {
+        gridElement.style.backgroundColor = "black";
+      },
+      { once: true } // this to remove the event after firing it.
+    );
+
     gridContainer.appendChild(gridElement);
   }
 }
